9bd5072df043236b5e7633ac3709cf3fc03a7e5a
fernandaaji <ajifernanda@gmail.com>
2024-01-15 21:30:12 +0700

    rubah halaman utama jadi index dan perbaiki responsive dengan mobile

--- index.php
+++ index.php
@@ -19,17 +19,17 @@
 </head>
 
 <body class="hold-transition login-page">
-    <div class="container-fluid flex bg-sky-600 flex-col justify-center items-center text-white p-5" style="height: 300px;">
-        <h1 class="font-weight-bold mb-3">Opticheck QR Code</h1>
-        <img src="app/dist/img/logoapp.png" alt="Logo App" style="width: 150px; height: 150px; border-radius: 100px;">
+    <div class="container-fluid flex bg-sky-600 flex-col justify-center items-center text-white p-3" style="min-height: 200px;">
+        <h1 class="font-weight-bold mb-2">Opticheck QR Code</h1>
+        <img src="app/dist/img/logoapp.png" alt="Logo App" style="width: 100px; height: 100px; border-radius: 50%;">
         <br>
-        <h5>"Presensi Siswa SMK Pancasila Purwodadi menggunakan QR Code"</h5>
+        <h5 class="text-center mt-2">"Presensi Siswa SMK Pancasila Purwodadi menggunakan QR Code"</h5>
     </div>
-    <div class="container mt-5">
-        <div class="row justify-content-lg-center">
-            <div class="col-md-6">
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-12 col-md-6 mb-3">
                 <div class="card">
-                    <div class="card-body">
+                    <div class="card-body text-center">
                         <i class="fa-solid fa-graduation-cap fa-fw mb-3 text-primary" style="font-size: 34px;"></i>
                         <h3>Siswa</h3>
                         <p class="card-text">Apabila anda adalah seorang Siswa, silahkan Login sebagai Siswa. Jika belum memiliki akun, silahkan lakukan registrasi terlebih dahulu.</p>
@@ -37,39 +37,37 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-12 col-md-6 mb-3">
                 <div class="card">
-                    <div class="card-body">
+                    <div class="card-body text-center">
                         <i class="fa-solid fa-chalkboard-user fa-fw mb-3 text-primary" style="font-size: 34px;"></i>
                         <h3>Guru</h3>
                         <p class="card-text">Apabila anda adalah seorang Guru, silahkan Login terlebih dahulu untuk melihat rekap data kehadiran siswa.</p>
-                        <div class="d-grid">
-                            <a href="pages/Guru/loginGuru.php" class="btn btn-primary btn-block">Login sebagai Guru</a>
-                        </div>
+                        <a href="pages/Guru/loginGuru.php" class="btn btn-primary btn-block">Login sebagai Guru</a>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-12 col-md-6 mb-3">
                 <div class="card">
-                    <div class="card-body">
+                    <div class="card-body text-center">
                         <i class="fa-solid fa-user-tie fa-fw mb-3 text-primary" style="font-size: 34px;"></i>
                         <h3>Admin</h3>
                         <p class="card-text">Apabila anda adalah seorang Admin, silahkan login untuk mengelola data</p>
-                        <div class="d-grid">
-                            <a href="pages/Admin/loginAdmin.php" class="btn btn-primary btn-block">Login sebagai Admin</a>
-                        </div>
+                        <a href="pages/Admin/loginAdmin.php" class="btn btn-primary btn-block">Login sebagai Admin</a>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- /.login-box -->
+    </div>
 
-        <!-- jQuery -->
-        <script src="app/plugins/jquery/jquery.min.js"></script>
-        <!-- Bootstrap 4 -->
-        <script src="app/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-        <!-- AdminLTE App -->
-        <script src="app/dist/js/adminlte.min.js"></script>
+    <!-- /.login-box -->
+
+    <!-- jQuery -->
+    <script src="app/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="app/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="app/dist/js/adminlte.min.js"></script>
 </body>
 
 </html>
