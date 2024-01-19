84e2488154c404578b0cbac4949e5ab0d90c44fc
fernandaaji <ajifernanda@gmail.com>
2024-01-20 06:53:09 +0700

    perbaikan tambah user

--- pages/dataUser/tambah_user_proses.php
+++ pages/dataUser/tambah_user_proses.php
@@ -1,19 +1,29 @@
 <?php
 include __DIR__ . "/../../conf/conn.php";
+
 if ($_POST) {
     $id = $_POST['id'];
     $nama = $_POST['nama'];
     $username = $_POST['username'];
-    $password = $_POST['password']; //md5($_POST['password']); untuk menyembunyikan password
+    $password = $_POST['password'];
     $role = $_POST['role'];
+    
     // Sesuaikan format tanggal dari input user (d-m-Y atau Y-m-d)
-    $created_date = date('Y-m-d', strtotime($_POST['created_date'])); //PERLU DIPERBAIKI KARENA JIKA INPUT DARI HALAMAN TAMBAH USER, TANGGAL BELUM BISA SESUAI ANTARA YANG DIPILIH DENGAN YANG TERSIMPAN
+    $created_date = date('Y-m-d');
 
-    $query = ("INSERT INTO user(id,nama,username,password,role,created_date) 
-           VALUES ('" . $id . "','" . $nama . "','" . $username . "','" . $password . "','" . $role . "','" . $created_date . "')");
-    if (!mysqli_query($conn, $query)) {
-        die(mysql_error);
+    // Gunakan parameterized query untuk mencegah SQL injection
+    $query = "INSERT INTO user (id, nama, username, password, role, created_date) 
+              VALUES (?, ?, ?, ?, ?, ?)";
+
+    $stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, "ssssss", $id, $nama, $username, $password, $role, $created_date);
+
+    if (!mysqli_stmt_execute($stmt)) {
+        die(mysqli_error($conn)); // Tangani kesalahan dengan memberikan pesan yang jelas
     } else {
         echo '<script>alert("Data Berhasil Ditambahkan !!!"); window.location.href="../Admin/indexAdmin.php"</script>';
     }
+
+    mysqli_stmt_close($stmt);
 }
+?>
