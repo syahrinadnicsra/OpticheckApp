3266c50f7ae900fa9d6d46c327e6adff000dca6c
syahrinadnicsra <syahrinadini1112@gmail.com>
2024-01-24 21:25:30 +0700

    Perbaikan

--- pages/Siswa/dashboardSiswa.php
+++ pages/Siswa/dashboardSiswa.php
@@ -1,227 +1,140 @@
 <?php
 
+require_once __DIR__ . "/../../conf/conn.php";
+
+// Dapatkan tanggal awal dan akhir bulan saat ini
+$today = getdate();
+$start_date = date('Y-m-01', strtotime($today['year'] . '-' . $today['mon'] . '-01'));
+$end_date = date('Y-m-t', strtotime($today['year'] . '-' . $today['mon'] . '-01'));
+
+// Hitung total hari pada bulan ini
+$total_days_in_month = cal_days_in_month(CAL_GREGORIAN, $today['mon'], $today['year']);
+
+if ($_SESSION['role'] === 'siswa') {
+    $nis_session = isset($_SESSION['nis']) ? $_SESSION['nis'] : '';
+    $query = "SELECT siswa.nis, siswa.nama_siswa, siswa.kelas, siswa.jurusan, COUNT(*) as total_hadir
+          FROM siswa
+          LEFT JOIN checkin ON siswa.nis = checkin.nis AND checkin.tanggal BETWEEN '$start_date' AND '$end_date'
+          WHERE siswa.nis = '$nis_session'
+          GROUP BY siswa.nis";
+} else {
+    $query = "SELECT siswa.nis, siswa.nama_siswa, siswa.kelas, siswa.jurusan, COUNT(*) as total_hadir
+          FROM siswa
+          LEFT JOIN checkin ON siswa.nis = checkin.nis AND checkin.tanggal BETWEEN '$start_date' AND '$end_date'
+          GROUP BY siswa.nis";
+}
+$result = $conn->query($query);
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Opticheck</title>
-
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="../../app/plugins/fontawesome-free/css/all.min.css">
-    <script src="https://kit.fontawesome.com/79ba4adbee.js" crossorigin="anonymous"></script>
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- Tempusdominus Bootstrap 4 -->
-    <link rel="stylesheet" href="../../app/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="../../app/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-    <!-- JQVMap -->
-    <link rel="stylesheet" href="../../app/plugins/jqvmap/jqvmap.min.css">
-    <!-- Theme style -->
-    <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="../../app/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-    <!-- Daterange picker -->
-    <link rel="stylesheet" href="../../app/plugins/daterangepicker/daterangepicker.css">
-    <!-- summernote -->
-    <link rel="stylesheet" href="../../app/plugins/summernote/summernote-bs4.min.css">
-    <!-- DataTables -->
-    <link rel="stylesheet" href="../../app/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="../../app/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
-    <link rel="stylesheet" href="../../app/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
-    <link rel="stylesheet" href="../../app/dist/css/adminlte.min.css">
-</head>
-
-<body class="hold-transition sidebar-mini layout-fixed">
-    <div class="wrapper">
-
-        <!-- Preloader -->
-        <!--<div class="preloader flex-column justify-content-center align-items-center">
-      <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
-    </div>-->
 
 
-        <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <div class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0">Dashboard Siswa</h1>
-                        </div><!-- /.col -->
-                        <div class="col-sm-6">
-                            <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                <li class="breadcrumb-item active">Dashboard Siswa</li>
-                            </ol>
-                        </div><!-- /.col -->
-                    </div><!-- /.row -->
-                </div><!-- /.container-fluid -->
-            </div>
-            <!-- /.content-header -->
-
-            <!-- Main content -->
-            <section class="content">
-                <div class="container-fluid">
-                    <!-- Small boxes (Stat box) -->
-                    <div class="row">
-                        <div class="col-lg-3 col-6">
-                            <!-- small box -->
-                            <div class="small-box bg-info">
-                                <div class="inner">
-                                    <h3>Buat QR</h3>
-
-                                    <p>Untuk presensi masuk</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="fa-solid fa-qrcode"></i>
-                                </div>
-                                <a href="?page=qrcode" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-                        <!-- ./col -->
-                        <div class="col-lg-3 col-6">
-                            <!-- small box -->
-                            <div class="small-box bg-success">
-                                <div class="inner">
-                                    <h3>80<sup style="font-size: 20px">%</sup></h3>
-
-                                    <p>Hadir</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="fa-solid fa-check-double"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-                        <!-- ./col -->
-                        <div class="col-lg-3 col-6">
-                            <!-- small box -->
-                            <div class="small-box bg-warning">
-                                <div class="inner">
-                                    <h3>3</h3>
-
-                                    <p>Izin/Pulang</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="fa-solid fa-person-booth"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-                        <!-- ./col -->
-                        <div class="col-lg-3 col-6">
-                            <!-- small box -->
-                            <div class="small-box bg-danger">
-                                <div class="inner">
-                                    <h3>2</h3>
-
-                                    <p>Tidak Hadir</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="fa-solid fa-person-circle-question"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-                        <!-- ./col -->
-                    </div>
-                    <!-- /.row -->
-
-
-                    <!-- /Setting Link Halaman -->
-                </div>
-            </section>
-            <!-- /Conten-->
-        </div>
-        <!-- /.content-wrapper -->
-        <footer class="main-footer">
-            <strong>Copyright &copy; 2023 <a href="https://adminlte.io">Syahrina Dini Caesara</a>.</strong>
-            <div class="float-right d-none d-sm-inline-block">
-                <b>Version</b> 3.2.0
-            </div>
-        </footer>
-
-        <!-- Control Sidebar -->
-        <aside class="control-sidebar control-sidebar-dark">
-
-            <!-- Control sidebar content goes here -->
-        </aside>
-        <!-- /.control-sidebar -->
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Dashboard Siswa</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Dashboard Siswa</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
     </div>
-    <!-- ./wrapper -->
+    <!-- /.content-header -->
 
-    <!-- jQuery -->
-    <script src="../../app/plugins/jquery/jquery.min.js"></script>
-    <!-- jQuery UI 1.11.4 -->
-    <script src="../../app/plugins/jquery-ui/jquery-ui.min.js"></script>
-    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-    <script>
-        $.widget.bridge('uibutton', $.ui.button)
-    </script>
-    <!-- Bootstrap 4 -->
-    <script src="../../app/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- ChartJS -->
-    <script src="../../app/plugins/chart.js/Chart.min.js"></script>
-    <!-- Sparkline -->
-    <script src="../../app/plugins/sparklines/sparkline.js"></script>
-    <!-- JQVMap -->
-    <script src="../../app/plugins/jqvmap/jquery.vmap.min.js"></script>
-    <script src="../../app/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
-    <!-- jQuery Knob Chart -->
-    <script src="../../app/plugins/jquery-knob/jquery.knob.min.js"></script>
-    <!-- daterangepicker -->
-    <script src="../../app/plugins/moment/moment.min.js"></script>
-    <script src="../../app/plugins/daterangepicker/daterangepicker.js"></script>
-    <!-- Tempusdominus Bootstrap 4 -->
-    <script src="../../app/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
-    <!-- Summernote -->
-    <script src="../../app/plugins/summernote/summernote-bs4.min.js"></script>
-    <!-- overlayScrollbars -->
-    <script src="../../app/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="../../app/dist/js/adminlte.js"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="../../app/dist/js/demo.js"></script>
-    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-    <script src="../../app/dist/js/pages/dashboard.js"></script>
-    <!-- DataTables  & Plugins -->
-    <script src="../../app/plugins/datatables/jquery.dataTables.min.js"></script>
-    <script src="../../app/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-    <script src="../../app/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-    <script src="../../app/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-    <script src="../../app/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-    <script src="../../app/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-    <script src="../../app/plugins/jszip/jszip.min.js"></script>
-    <script src="../../app/plugins/pdfmake/pdfmake.min.js"></script>
-    <script src="../../app/plugins/pdfmake/vfs_fonts.js"></script>
-    <script src="../../app/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-    <script src="../../app/plugins/datatables-buttons/js/buttons.print.min.js"></script>
-    <script src="../../app/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
-    <script>
-        $(function() {
-            $("#example1").DataTable({
-                "responsive": true,
-                "lengthChange": false,
-                "autoWidth": false,
-                "buttons": ["excel", "pdf", "print"]
-            }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-            $('#obat').DataTable({
-                "paging": true,
-                "lengthChange": false,
-                "searching": false,
-                "ordering": true,
-                "info": true,
-                "autoWidth": false,
-                "responsive": true,
-            });
-        });
-    </script>
-</body>
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>Buat QR</h3>
 
-</html>
+                            <p>Untuk presensi masuk</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fa-solid fa-qrcode"></i>
+                        </div>
+                        <a href="?page=qrcode" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <?php
+                            while ($row = $result->fetch_assoc()) {
+                                $attendance_percentage = ($total_days_in_month > 0) ? ($row['total_hadir'] / $total_days_in_month) * 100 : 0;
+                                echo "<h3>" . number_format($attendance_percentage, 2) . "<sup style=\"font-size: 20px\">%</sup></h3>";
+                            }
+                            ?>
+
+
+                            <p>Hadir</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fa-solid fa-check-double"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-warning">
+                        <div class="inner">
+                            <h3>3</h3>
+
+                            <p>Izin</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fa-solid fa-person-booth"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-danger">
+                        <div class="inner">
+                            <h3>2</h3>
+
+                            <p>Tidak Hadir</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fa-solid fa-person-circle-question"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+            </div>
+            <!-- /.row -->
+
+
+            <!-- /Setting Link Halaman -->
+        </div>
+    </section>
+    <!-- /Conten-->
+</div>
+<!-- /.content-wrapper -->
+
+<!-- Control Sidebar -->
+
+
+<!-- Control sidebar content goes here -->
+
+<!-- /.control-sidebar -->
+<!--</div>-->
+<!-- ./wrapper -->
